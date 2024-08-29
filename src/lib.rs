417aa83a8694ac3465c0dec872909d0727beb71a
extrawurst <mail@rusticorn.com>
2024-08-29 20:14:47 +0800

    more more docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,25 @@ mod web;
 
 pub use plugin::{WebAlertResponse, WebAlertsPlugin};
 
+/// Show a textinput dialog using DOM elements and communicate back to the bevy side via Events `WebAlertResponse`.
+///
+/// It will spawn the following DOM structure at the root of <body>:
+///
+/// ```html
+/// <div class="bevy_wasm_popup_root">
+///     <div class="bevy_wasm_popup_popup">
+///         <strong class="bevy_wasm_popup_label">title</strong>
+///         <input type="text" class="bevy_wasm_popup_text">
+///         <div class="bevy_wasm_popup_buttons">
+///             <button class="bevy_wasm_popup_button">button_label_cancel</button>
+///             <button class="bevy_wasm_popup_button">button_label_ok</button>
+///         </div>
+///     </div>
+/// </div>
+/// ```
+///
+/// It will also register the button on click handlers.
+/// After either button is pressed it will remove all these DOM elements again and send the last value of the input field back to us.
 #[allow(clippy::missing_const_for_fn, unused_variables)]
 pub fn show_textinput(
     title: &str,
@@ -20,6 +39,7 @@ pub fn show_textinput(
     }
 }
 
+/// show the browsers standard `window.alert()` using the provided `msg`.
 #[allow(clippy::missing_const_for_fn, unused_variables)]
 pub fn alert(msg: &str) {
     #[cfg(target_family = "wasm")]
